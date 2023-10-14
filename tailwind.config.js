24684607a6982fc4db4a4b9b6891d9884dad28da
Utsav173 <81065457+Utsav173@users.noreply.github.com>
2023-10-14 11:00:57 +0530

    change in themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,36 +18,24 @@ module.exports = {
     daisyui: {
       themes: [
         'light',
-        'dark',
         'cupcake',
         'bumblebee',
         'emerald',
         'corporate',
-        'synthwave',
-        'retro',
         'cyberpunk',
-        'valentine',
-        'halloween',
         'garden',
-        'forest',
-        'aqua',
         'lofi',
         'pastel',
         'fantasy',
         'wireframe',
         'black',
-        'luxury',
-        'dracula',
         'cmyk',
         'autumn',
-        'business',
         'acid',
         'lemonade',
-        'night',
-        'coffee',
-        'winter',
+        'winter'
       ], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-      darkTheme: 'dark', // name of one of the included themes for dark mode
+      darkTheme: 'black', // name of one of the included themes for dark mode
       base: true, // applies background color and foreground color for root element by default
       styled: true, // include daisyUI colors and design decisions for all components
       utils: true, // adds responsive and modifier utility classes
