6077cdde4ee7812667edd51c3d084113c848e6f8
Utsav173 <81065457+Utsav173@users.noreply.github.com>
2023-10-14 11:06:14 +0530

    refix theme issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -40,34 +40,22 @@ export default function Navbar() {
   const changeTheme = () => {
     const randomTheme = [
       'light',
-      'dark',
       'cupcake',
       'bumblebee',
       'emerald',
       'corporate',
-      'synthwave',
-      'retro',
       'cyberpunk',
-      'valentine',
-      'halloween',
       'garden',
-      'forest',
-      'aqua',
       'lofi',
       'pastel',
       'fantasy',
       'wireframe',
       'black',
-      'luxury',
-      'dracula',
       'cmyk',
       'autumn',
-      'business',
       'acid',
       'lemonade',
-      'night',
-      'coffee',
-      'winter',
+      'winter'
     ];
 
     const currentTheme = document.documentElement.dataset.theme;
