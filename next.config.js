7d5e0e78d50470a67fac0a6ac8d1facdec83634d
utsav173 <khatriutsav40@gmail.com>
2024-06-02 11:26:25 +0530

    rm react rc

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        pathname: '**',
-      },
-    ],
-  },
   experimental: {
-    reactCompiler: true,
     ppr: 'incremental',
   },
 };
