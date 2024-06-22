278dde8d15b893e90d4d37e1231280c5abee14a6
Utsav173 <khatri40@gmail.com>
2024-06-22 20:09:45 +0530

    added mdx next 15 version to blogSync

--- src/app/blog/[id]/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  // Or a custom loading skeleton component
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <span className="loading loading-infinity loading-lg"></span>
-    </div>
-  );
-}
